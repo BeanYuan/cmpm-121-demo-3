45f4b5c5ba0835d1a992c987925261365edda154
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-11-16 17:45:19 -0800

    D3.b complete

--- src/main.ts
+++ src/main.ts
@@ -39,41 +39,77 @@ let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
 
+function latLngToGlobalCoords(lat: number, lng: number) {
+    return {
+        i: Math.floor(lat * 10000),
+        j: Math.floor(lng * 10000)
+    };
+}
+
+const gridCellFactory = (function() {
+    interface Cell {
+        i: number;
+        j: number;
+    }
+
+    interface CellCache {
+        [key: string]: Cell;
+    }
+
+    const cellCache: CellCache = {};
+
+    return {
+        getCell: function(i: number, j: number): Cell {
+            const key = `${i}-${j}`;
+            if (!cellCache[key]) {
+                cellCache[key] = { i, j };
+            }
+            return cellCache[key];
+        }
+    };
+})();
+
+function createCoin(i: any, j: any, serial: any) {
+    return { i, j, serial };
+}
+
 function makePit(i: number, j: number) {
+    const globalCoords = latLngToGlobalCoords(MERRILL_CLASSROOM.lat + i * TILE_DEGREES, MERRILL_CLASSROOM.lng + j * TILE_DEGREES);
+    const cell = gridCellFactory.getCell(globalCoords.i, globalCoords.j);
+    
     const bounds = leaflet.latLngBounds([
-        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
-        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES,
-        MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
+        [MERRILL_CLASSROOM.lat + i * TILE_DEGREES, MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
+        [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES, MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
     ]);
 
-    const pit = leaflet.rectangle(bounds) as leaflet.Layer;
-
-    let coins = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+    const pit = leaflet.rectangle(bounds);
+    let coins: { i: any; j: any; serial: any; }[] = [];
+    for (let serial = 0; serial < luck([i, j, "initialValue"].toString()) * 100; serial++) {
+        coins.push(createCoin(cell.i, cell.j, serial));
+    }
 
     pit.bindPopup(() => {
         const container = document.createElement("div");
-        container.innerHTML = `
-                <div>There is a pit here at "${i},${j}". It has <span id="coins">${coins}</span> geocoins.</div>
-                <button id="collect">Collect</button>
-                <button id="deposit">Deposit</button>`;
+        container.innerHTML = `<div>There is a pit here at "${cell.i},${cell.j}". It has <span id="coins">${coins.length}</span> geocoins.</div>
+                               <button id="collect">Collect</button>
+                               <button id="deposit">Deposit</button>`;
         const collectButton = container.querySelector<HTMLButtonElement>("#collect")!;
         const depositButton = container.querySelector<HTMLButtonElement>("#deposit")!;
 
         collectButton.addEventListener("click", () => {
-            if (coins > 0) {
-                coins--;
+            if (coins.length > 0) {
+                coins.pop(); // Remove one coin
                 points++;
-                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.toString();
+                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.length.toString();
                 statusPanel.innerHTML = `${points} geocoins collected`;
             }
         });
 
         depositButton.addEventListener("click", () => {
             if (points > 0) {
-                coins++;
+                coins.push(createCoin(cell.i, cell.j, coins.length)); // Add a new coin
                 points--;
-                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.toString();
+                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.length.toString();
                 statusPanel.innerHTML = `${points} geocoins remaining`;
             }
         });
