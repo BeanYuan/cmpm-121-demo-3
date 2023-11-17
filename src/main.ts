acefae6248386105ba11fd5600c19353422f43f3
BeanYuan <65484019+BeanYuan@users.noreply.github.com>
2023-11-16 18:41:38 -0800

    D3.c Complete

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 import leaflet from "leaflet";
 import luck from "./luck";
 import "./leafletWorkaround";
+import L from "leaflet";
 
 
 const MERRILL_CLASSROOM = leaflet.latLng({
@@ -14,6 +15,7 @@ const GAMEPLAY_ZOOM_LEVEL = 19;
 const TILE_DEGREES = 1e-4;
 const NEIGHBORHOOD_SIZE = 12;
 const PIT_SPAWN_PROBABILITY = 0.1;
+const MOVE_STEP = TILE_DEGREES * 2;
 
 const mapContainer = document.querySelector<HTMLElement>("#map")!;
 
@@ -38,6 +40,35 @@ playerMarker.addTo(map);
 let points = 0;
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
 statusPanel.innerHTML = "No points yet...";
+
+class PitMemento {
+    constructor(public coins: any) {}
+}
+  
+class PitState {
+    memento: PitMemento;
+    constructor(public i: any, public j: any, public initialCoins: any) {
+        this.memento = new PitMemento(initialCoins);
+    }
+
+    get coins() {
+        return this.memento.coins;
+    }
+
+    set coins(value) {
+        this.memento.coins = value;
+    }
+
+    saveState() {
+        this.memento = new PitMemento(this.coins);
+    }
+
+    restoreState() {
+        this.coins = this.memento.coins;
+    }
+}
+  
+const pitStates = new Map();
 
 function latLngToGlobalCoords(lat: number, lng: number) {
     return {
@@ -81,43 +112,89 @@ function makePit(i: number, j: number) {
         [MERRILL_CLASSROOM.lat + i * TILE_DEGREES, MERRILL_CLASSROOM.lng + j * TILE_DEGREES],
         [MERRILL_CLASSROOM.lat + (i + 1) * TILE_DEGREES, MERRILL_CLASSROOM.lng + (j + 1) * TILE_DEGREES],
     ]);
-
+  
     const pit = leaflet.rectangle(bounds);
-    let coins: { i: any; j: any; serial: any; }[] = [];
-    for (let serial = 0; serial < luck([i, j, "initialValue"].toString()) * 100; serial++) {
-        coins.push(createCoin(cell.i, cell.j, serial));
+  
+    let pitState: PitState;
+    const key = `${i}-${j}`;
+    if (pitStates.has(key)) {
+      pitState = pitStates.get(key);
+      pitState.restoreState();
+    } else {
+        const initialCoinCount = Math.floor(luck([i, j, "initialValue"].toString()) * 100);
+        const initialCoins = [];
+        for (let serial = 0; serial < initialCoinCount; serial++) {
+            initialCoins.push(createCoin(cell.i, cell.j, serial));
+        }
+        pitState = new PitState(i, j, initialCoins);
+        pitStates.set(key, pitState);
     }
-
+  
     pit.bindPopup(() => {
         const container = document.createElement("div");
-        container.innerHTML = `<div>There is a pit here at "${cell.i},${cell.j}". It has <span id="coins">${coins.length}</span> geocoins.</div>
+        container.innerHTML = `<div>There is a pit here at "${cell.i},${cell.j}". It has <span id="coins">${pitState.coins.length}</span> geocoins.</div>
                                <button id="collect">Collect</button>
                                <button id="deposit">Deposit</button>`;
         const collectButton = container.querySelector<HTMLButtonElement>("#collect")!;
         const depositButton = container.querySelector<HTMLButtonElement>("#deposit")!;
-
+        
         collectButton.addEventListener("click", () => {
-            if (coins.length > 0) {
-                coins.pop(); // Remove one coin
+            if (pitState.coins.length > 0) {
+                pitState.coins.pop();
                 points++;
-                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.length.toString();
+                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = pitState.coins.length.toString();
                 statusPanel.innerHTML = `${points} geocoins collected`;
             }
         });
-
+    
         depositButton.addEventListener("click", () => {
             if (points > 0) {
-                coins.push(createCoin(cell.i, cell.j, coins.length)); // Add a new coin
+                pitState.coins.push(createCoin(cell.i, cell.j, pitState.coins.length));
                 points--;
-                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins.length.toString();
+                container.querySelector<HTMLSpanElement>("#coins")!.innerHTML = pitState.coins.length.toString();
                 statusPanel.innerHTML = `${points} geocoins remaining`;
             }
         });
-
+  
         return container;
     });
+  
     pit.addTo(map);
 }
+
+function movePlayer(latOffset: number, lngOffset: number) {
+    const currentPos = playerMarker.getLatLng();
+    playerMarker.setLatLng([currentPos.lat + latOffset, currentPos.lng + lngOffset]);
+    map.panTo([currentPos.lat + latOffset, currentPos.lng + lngOffset]);
+    regenerateCaches();
+}
+  
+function regenerateCaches() {
+    map.eachLayer(layer => {
+        if (layer !== playerMarker && !(layer instanceof L.TileLayer)) {
+            map.removeLayer(layer);
+        }
+    });
+
+    const playerPos = playerMarker.getLatLng();
+    const iStart = Math.floor((playerPos.lat - MERRILL_CLASSROOM.lat) / TILE_DEGREES) - NEIGHBORHOOD_SIZE;
+    const jStart = Math.floor((playerPos.lng - MERRILL_CLASSROOM.lng) / TILE_DEGREES) - NEIGHBORHOOD_SIZE;
+
+    for (let i = iStart; i < iStart + NEIGHBORHOOD_SIZE * 2; i++) {
+        for (let j = jStart; j < jStart + NEIGHBORHOOD_SIZE * 2; j++) {
+            if (luck([i, j].toString()) < PIT_SPAWN_PROBABILITY) {
+                makePit(i, j);
+            }
+        }
+    }
+}
+
+//regenerateCaches();
+
+document.getElementById("north")!.addEventListener("click", () => movePlayer(MOVE_STEP, 0));
+document.getElementById("south")!.addEventListener("click", () => movePlayer(-MOVE_STEP, 0));
+document.getElementById("east")!.addEventListener("click", () => movePlayer(0, MOVE_STEP));
+document.getElementById("west")!.addEventListener("click", () => movePlayer(0, -MOVE_STEP));
 
 for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
     for (let j = - NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
